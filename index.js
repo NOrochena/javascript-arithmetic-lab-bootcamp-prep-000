13cd6275d8e066bd708b72234011ea2eb20a577c
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-25 23:27:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function add(a,b){
 }
 
 function substract(a,b){
-  return b - a
+  return a-b
 }
 
 function multiply(a,b){
   return a*b
 }
 
-function devide(a, b){
+function divide(a, b){
   return a / b
 }
